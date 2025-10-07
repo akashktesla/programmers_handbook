b1136bea58bf0da79c5d1f4dc9da74425d350807
akashktesla <akashktesla@gmail.com>
2025-10-07 08:02:41 +0530

    :)

--- graph_theory/bfs.py
+++ graph_theory/bfs.py
@@ -16,6 +16,21 @@ def bfs(node,graph,visited,q):
     return prev
 
 
+def reconstruct_path(start,target,prev):
+    current = target
+    prev[start] = None
+    path = []
+    while current is not None:
+        path.append(current)
+        current = prev[current]
+    path.reverse()
+    if path[0] == start:
+        return path 
+    else:
+        return []
+
+
+
 
 if __name__ == "__main__":
     graph = {
@@ -23,7 +38,7 @@ if __name__ == "__main__":
             1: [3, 4],
             2: [5, 6],
             3: [7],
-            4: [0, 5],
+            4: [5,0],
             5: [8],
             6: [5, 9],
             7: [],
@@ -36,4 +51,6 @@ if __name__ == "__main__":
     q = deque()
     prev = bfs(node,graph,visited,q)
     print(f"Prev: {prev}")
+    path = reconstruct_path(1,8,prev)
+    print(f"path: {path}")
 
